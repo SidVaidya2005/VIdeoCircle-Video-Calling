280778159c65965032ce853c89faa3dcd9f061db
SidVaidya2005 <siddarthvaidya2005@gmail.com>
2026-04-26 22:53:58 +0530

    fix: update import path for VideoMeetComponent to correct location

--- frontend/src/app/routes.jsx
+++ frontend/src/app/routes.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from '../pages/landing';
 import Authentication from '../features/auth/pages/AuthPage';
-import VideoMeetComponent from '../pages/VideoMeet';
+import VideoMeetComponent from '../features/meet/pages/MeetPage';
 import HomeComponent from '../pages/home';
 import GuestHome from '../pages/joinmeet';
 import History from '../pages/history';
